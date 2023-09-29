b0f15980929b239b6ff572841a7b11ae7d372778
cesar3435ru <cesssuperbro3435@gmail.com>
2023-09-28 18:10:28 -0600

    mostrar productos con su nombre y precio

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,6 +8,17 @@ interface productslide {
   imagen: string,
 
 }
+interface product {
+  id: number,
+  precio: number,
+  stock: number,
+  name: string,
+  img: string,
+  description: string,
+  state: boolean,
+  code: string,
+
+}
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -22,6 +33,10 @@ export class Tab1Page {
   swiperSlideChanged(e: any) {
     console.log('Changed', e);
   }
+  onSwiperInit(event: any) {
+    event.autoplay.start();
+  }
+
 
   masVendidos: productslide[] = [
     {
@@ -43,6 +58,51 @@ export class Tab1Page {
     {
       id: 5,
       imagen: 'https://images6.alphacoders.com/684/684831.jpg'
+    },
+  ]
+
+  misProductos: product[] = [
+
+    {
+      id: 1,
+      name: "Galletas de animalito",
+      description: "Las mas buenas",
+      code: "c453g",
+      precio: 100,
+      stock: 10,
+      state: true,
+      img: 'https://chedrauimx.vtexassets.com/arquivos/ids/18990294-1600-auto?v=638302163026430000&width=1600&height=auto&aspect=true'
+    },
+
+    {
+      id: 2,
+      name: "Sabritas",
+      description: "Las mejores",
+      code: "c553a",
+      precio: 200,
+      stock: 0,
+      state: true,
+      img: 'https://www.tasteboutique.com/cdn/shop/products/Sabritas_con_sal_170_gr_540x.jpg?v=1586040425'
+    },
+    {
+      id: 3,
+      name: "Takis",
+      description: "Las mejores",
+      code: "ta530",
+      precio: 300,
+      stock: 0,
+      state: true,
+      img: 'https://chedrauimx.vtexassets.com/arquivos/ids/19701968-1600-auto?v=638315289761330000&width=1600&height=auto&aspect=true'
+    },
+    {
+      id: 4,
+      name: "Celular",
+      description: "Las mejores",
+      code: "ma453",
+      precio: 10000,
+      stock: 17,
+      state: true,
+      img: 'https://m.media-amazon.com/images/I/61wVdpwmktL._AC_SL1500_.jpg'
     },
   ]
 
