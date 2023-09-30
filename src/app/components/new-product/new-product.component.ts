10b47ccef9f426ae7e57271109fefb8de285187a
cesar3435ru <cesssuperbro3435@gmail.com>
2023-09-29 22:21:48 -0600

    Formulario de agregar producto con validaciones

--- src/app/components/new-product/new-product.component.ts
+++ src/app/components/new-product/new-product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, NavController, ToastController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-product',
@@ -8,12 +9,57 @@ import { ModalController } from '@ionic/angular';
 })
 export class NewProductComponent implements OnInit {
 
-  constructor(private modalC: ModalController) { }
+  constructor(private modalC: ModalController, private navCtrl: NavController,
+    private formb: FormBuilder, private toastController: ToastController) { }
 
   ngOnInit() { }
+
+  categories = ['Abarrotes', 'Limpieza', 'Golosinas', 'Vinos y licores', 'Frutas y verduras', 'Panaderia']
 
   async closeModal() {
     await this.modalC.dismiss();
   }
 
+
+  productForm: FormGroup = this.formb.group({
+    name: ['', [Validators.required, Validators.maxLength(20)]],
+    codeproduct: ['', [Validators.required, Validators.maxLength(5), Validators.minLength(5)]],
+    description: ['', [Validators.required, Validators.maxLength(100)]],
+    price: [null, [Validators.required, Validators.min(1), Validators.max(5000)]],
+    // inventory: [null, [Validators.required, Validators.min(1),Validators.max(5000), Validators.pattern(this.priceP)]],
+    category_id: ['', [Validators.required]],
+    stock: [null, [Validators.required, Validators.min(1), Validators.max(1000)]],
+    urlimg: ['', [Validators.required, Validators.maxLength(200)]],
+    // mark: ['', [Validators.required, Validators.maxLength(30)]],
+    adquisicion: ['', Validators.required],
+    vencimiento: ['', Validators.required],
+
+  })
+
+
+
+  validarInput(campo: string) {
+    return this.productForm.controls[campo].errors && this.productForm.controls[campo].touched
+  }
+
+  saveProduct() {
+    if (this.productForm.valid) {
+      console.log(this.productForm.value);
+      this.productForm.reset();
+    }
+  }
+
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Product added successfully!',
+      duration: 1500,
+      position: position,
+      icon: 'checkmark-done-outline',
+      color: 'success'
+    });
+
+    await toast.present();
+  }
+
 }
+
